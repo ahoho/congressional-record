44538bae66dcc6f984a96a0fec26d618732195a1
Alexander Hoyle <ahoyle@wesleyan.edu>
2021-01-15 19:59:27 -0500

    Create download_legislator_data.sh

---
+++ download_legislator_data.sh
@@ -0,0 +1,2 @@
+wget https://theunitedstates.io/congress-legislators/legislators-historical.json -O data/legislators-historical.json
+wget https://theunitedstates.io/congress-legislators/legislators-current.json -O data/legislators-current.json
